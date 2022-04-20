b4aace004da5075d45cccb27b5a0adac62ef1843
Novacovici Sonia <sonianovacovici@gmail.com>
2022-04-20 20:27:13 +0300

    phone validation & password encoding added to register

--- src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/RegisterController.java
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/controllers/RegisterController.java
@@ -32,10 +32,12 @@ public class RegisterController {
     @FXML
     public void handleRegisterAction() {
         try {
-            if (usernameField.getText() == "" || passwordField.getText() == "" || (String) role.getValue() == "" || fullNameField.getText() == "" || phoneNumberField.getText() == "") {
+            if (usernameField.getText() == "" || passwordField.getText() == "" || (String) role.getValue() == null || fullNameField.getText() == "" || phoneNumberField.getText() == "") {
                 registrationMessage.setText("Please fill in all the fields!");
-            } else {
-                UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue(), fullNameField.getText(), phoneNumberField.getText());
+            } else if (UserService.validatePhoneNumber(phoneNumberField.getText()) == false) {
+                registrationMessage.setText("Invalid phone number!");
+            }else {
+                UserService.addUser(usernameField.getText(), UserService.encodePassword(passwordField.getText()), (String) role.getValue(), fullNameField.getText(), phoneNumberField.getText());
                 registrationMessage.setText("Account created successfully!");
             }
         } catch (SQLException e) {
--- src/main/java/org/loose/fis/sre/hotelreservationapplication/services/UserService.java
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/services/UserService.java
@@ -1,9 +1,12 @@
 package org.loose.fis.sre.hotelreservationapplication.services;
 
 import org.loose.fis.sre.hotelreservationapplication.database.DBConnection;
-
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
+import java.io.*;
+import java.util.regex.*;
 
 public class UserService {
 
@@ -11,6 +14,29 @@ public class UserService {
 
         Statement statement = DBConnection.connection.createStatement();
         statement.executeUpdate("INSERT INTO user (username, password, role, fullName, phoneNumber) VALUES ('" + username + "', '" + password + "', '" + role + "', '" + fullName + "', '" + phoneNumber + "');");
+    }
+
+    public static String encodePassword (String password) {
+        String encryptedpassword = null;
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(password.getBytes());
+            byte[] bytes = m.digest();
+            StringBuilder s = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++) {
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            encryptedpassword = s.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encryptedpassword;
+    }
+
+    public static boolean validatePhoneNumber (String phone) {
+        Pattern p = Pattern.compile("^(\\d{3}[- .]?){2}\\d{4}$");
+        Matcher m = p.matcher(phone);
+        return (m.matches());
     }
 
 }
