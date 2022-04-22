6efe068cf87375c6d6003318e85b499ea5a7ab20
Ioana-Novacovici <ioananovacovici@gmail.com>
2022-04-22 23:42:13 +0300

    added login to HRA

--- src/main/java/org/loose/fis/sre/hotelreservationapplication/Main.java
+++ src/main/java/org/loose/fis/sre/hotelreservationapplication/Main.java
@@ -2,6 +2,7 @@ package org.loose.fis.sre.hotelreservationapplication;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.loose.fis.sre.hotelreservationapplication.database.DBConnection;
@@ -9,15 +10,24 @@ import org.loose.fis.sre.hotelreservationapplication.database.DBConnection;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Stage stg;
+
     @Override
     public void start(Stage stage) throws IOException {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("register.fxml"));
+        stg = stage;
+        stage.setResizable(false);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hotel Reservation");
+        stage.setTitle("Hotel Reservation Application");
         stage.setScene(scene);
         stage.show();
 
+    }
+
+    public void switchScene(String fxml) throws IOException{
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
